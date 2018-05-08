a94e38329a2ff1b420cd12a3327e702a327e87c3
avkudr <argon24@mail.ru>
2018-05-08 14:40:50 +0200

    rebuild pages

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -e 's#url: \".\"#url: \"https://avkudr.github.io\"#' -i '' _config.yml
+sed -e '10s#url: \".\"#url: \"https://avkudr.github.io\"#' -i '' _config.yml
 jekyll build
 cd _site
 git add -A
 git commit -m 'rebuild pages' --allow-empty
 git push origin master
 cd ..
-sed -e 's#url: \"https://avkudr.github.io\"#url: \".\"#' -i '' _config.yml
+sed -e '10s#url: \"https://avkudr.github.io\"#url: \".\"#' -i '' _config.yml
