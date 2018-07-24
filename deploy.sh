c41af021b884bcca126401c2dd1d74597f6d64d6
avkudr <argon24@mail.ru>
2018-07-24 02:02:56 +0200

    rebuild pages

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,29 @@
 # build the site and put it online
 # command:
 #   npm run deploy
-sed -e '10s#url: \".\"#url: \"https://avkudr.github.io\"#' -i '' _config.yml
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [unameOut = "Darwin"]
+then 
+    sed -e '10s#url: \".\"#url: \"https://avkudr.github.io\"#' -i '' _config.yml
+fi
+
 jekyll build
 cd _site
 git add -A
 git commit -m 'rebuild pages' --allow-empty
 git push origin master
 cd ..
-sed -e '10s#url: \"https://avkudr.github.io\"#url: \".\"#' -i '' _config.yml
+
+if [unameOut = "Darwin"]
+then 
+    sed -e '10s#url: \"https://avkudr.github.io\"#url: \".\"#' -i '' _config.yml
+fi
